987bdc391fdad379b45ba5c5c0b07f478b586112
Strikelab <contact@strikelab.fr>
2023-11-23 15:38:43 +0100

    update services content

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -13,10 +13,10 @@ function Services() {
             </div>
             <h3 className="service__item__title">Web devlopment</h3>
             <p className="service__item__description">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
-              unde accusantium perspiciatis doloribus, cupiditate nesciunt
-              molestiae nisi, et explicabo consequuntur tenetur quaerat dolore
-              fugit impedit, harum esse. Provident, officiis culpa?
+              Offering cutting-edge web development solutions tailored to your
+              unique needs. From front-end design using technologies like React
+              and Redux to robust back-end development with Node.js and PHP, we
+              create seamless, user-friendly websites.
             </p>
           </div>
         </div>
@@ -27,10 +27,11 @@ function Services() {
             </div>
             <h3 className="service__item__title">SEO Optimization</h3>
             <p className="service__item__description">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
-              unde accusantium perspiciatis doloribus, cupiditate nesciunt
-              molestiae nisi, et explicabo consequuntur tenetur quaerat dolore
-              fugit impedit, harum esse. Provident, officiis culpa?
+              Elevate your online presence with our comprehensive SEO
+              optimization services. We implement proven strategies to enhance
+              your website's visibility on search engines, driving organic
+              traffic and ensuring your business stands out in the digital
+              landscape.
             </p>
           </div>
         </div>
@@ -39,12 +40,12 @@ function Services() {
             <div className="service__item__icon">
               <i className="fa-solid fa-network-wired" aria-hidden="true"></i>
             </div>
-            <h3 className="service__item__title">Network</h3>
+            <h3 className="service__item__title">Network Solutions</h3>
             <p className="service__item__description">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
-              unde accusantium perspiciatis doloribus, cupiditate nesciunt
-              molestiae nisi, et explicabo consequuntur tenetur quaerat dolore
-              fugit impedit, harum esse. Provident, officiis culpa?
+              Building secure, efficient, and scalable network infrastructures.
+              Whether establishing a new network or enhancing an existing one,
+              our expertise in Apache, Debian, and networking technologies
+              ensures reliable connectivity and performance.
             </p>
           </div>
         </div>
@@ -55,10 +56,10 @@ function Services() {
             </div>
             <h3 className="service__item__title">Formation</h3>
             <p className="service__item__description">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
-              unde accusantium perspiciatis doloribus, cupiditate nesciunt
-              molestiae nisi, et explicabo consequuntur tenetur quaerat dolore
-              fugit impedit, harum esse. Provident, officiis culpa?
+              Empower your team with our tailored training programs. From
+              hands-on workshops in Micropython and Raspberry Pi to
+              comprehensive courses on web development technologies like MongoDB
+              and React, we equip your team with the skills needed for success.
             </p>
           </div>
         </div>
@@ -69,10 +70,10 @@ function Services() {
             </div>
             <h3 className="service__item__title">Debug</h3>
             <p className="service__item__description">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
-              unde accusantium perspiciatis doloribus, cupiditate nesciunt
-              molestiae nisi, et explicabo consequuntur tenetur quaerat dolore
-              fugit impedit, harum esse. Provident, officiis culpa?
+              Swiftly resolve issues with our expert debugging. Utilizing tools
+              like Jira, we analyze code, troubleshoot errors, optimize
+              performance. Our goal is to ensure your software operates
+              seamlessly, delivering an enhanced user experience.
             </p>
           </div>
         </div>
@@ -83,10 +84,10 @@ function Services() {
             </div>
             <h3 className="service__item__title">Design</h3>
             <p className="service__item__description">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
-              unde accusantium perspiciatis doloribus, cupiditate nesciunt
-              molestiae nisi, et explicabo consequuntur tenetur quaerat dolore
-              fugit impedit, harum esse. Provident, officiis culpa?
+              Transform your ideas into visually stunning realities with our
+              design expertise. From creating captivating user interfaces using
+              Figma to graphic design with Gimp, we bring creativity and
+              functionality together to deliver impactful design solutions.
             </p>
           </div>
         </div>
