397207d97b56a268bc33492e892cadc3e6d6b7a8
Strikelab <contact@strikelab.fr>
2023-11-25 01:06:31 +0100

    close burger menu on links click feature

--- src/components/Burger.jsx
+++ src/components/Burger.jsx
@@ -1,25 +1,41 @@
-import { push as Menu } from "react-burger-menu";
+import React, { useState } from "react";
+import { bubble as Menu } from "react-burger-menu";
 import { Link } from "react-router-dom";
 import { HashLink } from "react-router-hash-link";
 
 function Burger() {
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+  const closeMenu = () => {
+    setMenuIsOpen(false);
+  };
+
   return (
     <>
-      <Menu width={"50%"} pageWrapId="page-wrap" outerContainerId="root" right>
-        <Link to="/" className="menu-item">
+      <Menu
+        isOpen={menuIsOpen}
+        width={"50%"}
+        pageWrapId="page-wrap"
+        outerContainerId="root"
+        right
+        // onStateChange prop is a callback that gets called whenever the state of the Menu
+        // component changes. The state object contains information about the menu,
+        // and in this case, we are interested in the isOpen property of that state.
+        onStateChange={(state) => setMenuIsOpen(state.isOpen)}
+      >
+        <Link to="#" className="menu-item" onClick={closeMenu}>
           Home
         </Link>
-        <HashLink to="#about" smooth className="menu-item">
+        <HashLink to="#about" className="menu-item" onClick={closeMenu}>
           About
         </HashLink>
-        <HashLink to="#" smooth className="menu-item" href="#info">
+        <HashLink to="#service" className="menu-item" onClick={closeMenu}>
           Service
         </HashLink>
-        <HashLink to="#projects" smooth className="menu-item" href="#info">
+        <HashLink to="#projects" className="menu-item" onClick={closeMenu}>
           Project
         </HashLink>
-
-        <HashLink to="#" smooth className="menu-item">
+        <HashLink to="#contact" className="menu-item" onClick={closeMenu}>
           Contact
         </HashLink>
       </Menu>
