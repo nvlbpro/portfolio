2a90624697f4aa210cee73c33ce4d74e6cad17f1
Strikelab <contact@strikelab.fr>
2023-11-26 18:23:17 +0100

    add the skill name on hover

--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -5,6 +5,7 @@ function Tile(props) {
   return (
     <div className="skills__tile">
       <img className="skills__picture" src={picture} alt={props.skill} />
+      <span className="skills__name">{props.skill}</span>
     </div>
   );
 }
