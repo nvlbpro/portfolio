80e0d6511b0298d791e56fa69a434cbaa554d5d9
Strikelab <contact@strikelab.fr>
2023-11-26 09:40:33 +0100

    correction bug on navbar menu link

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,15 @@
 // import Burger from "./Burger";
+import { useState } from "react";
 import Logo from "./Logo";
 import Menu from "./Menu";
 
 function NavBar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <div id="navbar" className="navbar custom-navbar">
       <Logo />
-      <Menu />
+      <Menu isOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
     </div>
   );
 }
-
 export default NavBar;
