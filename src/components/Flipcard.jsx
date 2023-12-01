94065bba75fbf037e9c7334a2ec8e577e1c47283
Strikelab <contact@strikelab.fr>
2023-12-01 15:54:04 +0100

    css adjustments.

--- src/components/Flipcard.jsx
+++ src/components/Flipcard.jsx
@@ -29,9 +29,6 @@ function Flipcard(props) {
       backCss="flipcard__back"
       frontComponent={
         <>
-          {/* <div className="flipcard__header">
-            <h3 className="flipcard__title">{props.title}</h3>
-          </div> */}
           <div className="flipcard__content">
             <img
               className="flipcard__front-cover"
@@ -45,25 +42,28 @@ function Flipcard(props) {
       backComponent={
         <>
           <img className="flipcard__back-cover" src={cover} alt={props.cover} />
-          <div className="flipcard__header">
-            {props.logoOC && (
-              <img className="logo-oc" src={logoOC} alt="logo openclassrooms" />
-            )}
-            <p>{props.headingText}</p>
+          <div className="flipcard__back-content">
+            <div className="flipcard__header">
+              {props.logoOC && (
+                <img
+                  className="logo-oc"
+                  src={logoOC}
+                  alt="logo openclassrooms"
+                />
+              )}
+              <p>{props.headingText}</p>
+            </div>
+            <h3>{props.title}</h3>
+            <ul>
+              {competences?.map((competence, key) => (
+                <li className="flipcard__competence" key={key}>
+                  {competence}
+                </li>
+              ))}
+            </ul>
+            <div className="flipcard__footer">{skillsLogos}</div>
           </div>
-          <h3>{props.title}</h3>
-
-          <ul>
-            {competences?.map((competence, key) => (
-              <li className="flipcard__competence" key={key}>
-                {competence}
-              </li>
-            ))}
-          </ul>
-
-          <div className="flipcard__footer">{skillsLogos}</div>
-
-          <div className="flipcard__links-container">
+          <div className="flipcard__back-links-container">
             <a href={props.repoGithub} target="_blank" rel="noreferrer">
               <Button className="button__github" buttonText="REPO GITHUB">
                 <i className="fa-brands fa-github" aria-hidden="true"></i>
